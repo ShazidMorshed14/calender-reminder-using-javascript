0a4455bbe3d09a13e1fdedd564963fb4e9d44486
ShazidMorshed14 <67098062+ShazidMorshed14@users.noreply.github.com>
2022-07-23 22:31:10 +0600

    second commit

--- index.js
+++ index.js
@@ -37,14 +37,11 @@ function load() {
   }
 
   const day = dt.getDate();
-  const month = dt.getMonth(); //it stars from 0 index
+  const month = dt.getMonth();
   const year = dt.getFullYear();
-  console.log(day, month, year);
 
   const firstDayOfMonth = new Date(year, month, 1);
-  const daysInMonth = new Date(year, month + 1, 0).getDate(); //0 means the last day of the previous month
-  console.log(daysInMonth);
-  console.log(firstDayOfMonth);
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
 
   const dateString = firstDayOfMonth.toLocaleDateString("en-us", {
     weekday: "long",
@@ -52,10 +49,7 @@ function load() {
     month: "numeric",
     day: "numeric",
   });
-  console.log(dateString); //finding the weekday on current month
-
-  const paddingDays = weekdays.indexOf(dateString.split(",")[0]); //finding how many padding days to give before
-  console.log(paddingDays);
+  const paddingDays = weekdays.indexOf(dateString.split(", ")[0]);
 
   document.getElementById("monthDisplay").innerText = `${dt.toLocaleDateString(
     "en-us",
@@ -68,17 +62,40 @@ function load() {
     const daySquare = document.createElement("div");
     daySquare.classList.add("day");
 
+    const dayString = `${month + 1}/${i - paddingDays}/${year}`;
+
     if (i > paddingDays) {
       daySquare.innerText = i - paddingDays;
-      daySquare.addEventListener("click", () =>
-        openModal(`${month + 1}/${i - paddingDays}/${year}`)
-      );
+      const eventForDay = events.find((e) => e.date === dayString);
+
+      if (i - paddingDays === day && nav === 0) {
+        daySquare.id = "currentDay";
+      }
+
+      if (eventForDay) {
+        const eventDiv = document.createElement("div");
+        eventDiv.classList.add("event");
+        eventDiv.innerText = eventForDay.title;
+        daySquare.appendChild(eventDiv);
+      }
+
+      daySquare.addEventListener("click", () => openModal(dayString));
     } else {
       daySquare.classList.add("padding");
     }
 
     calendar.appendChild(daySquare);
   }
+}
+
+function closeModal() {
+  eventTitleInput.classList.remove("error");
+  newEventModal.style.display = "none";
+  deleteEventModal.style.display = "none";
+  backDrop.style.display = "none";
+  eventTitleInput.value = "";
+  clicked = null;
+  load();
 }
 
 function closeModal() {
